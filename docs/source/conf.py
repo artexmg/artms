27510935dba0fa728fa0edab926789073f99491c
AMG <artexmg@users.noreply.github.com>
2021-05-11 20:53:30 -0500

    fix: added mock for dependencies in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ["numpy", "dotenv", "dask", "pandas", "numpy", "boto3", "pytest","luigi","csci_utils"]
